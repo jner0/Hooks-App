ce9d5d062a76cc9a5cc028100b28f5ee1e406728
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-02-23 22:14:00 -0500

    Usando useLayoutEffect

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,43 @@
+import { Quote, LoadingQuote } from "../03-examples/index";
+import { useCounter, useFetch } from "../../hooks";
+
+
+export const Layout = () => {
+  
+    const { counter, increment } = useCounter(1);
+    const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
+    console.log(counter)
+    const { author, quote } = !!data && data[0];
+    
+    return (
+    <>
+        <h1>Breaking Bad Quotes </h1>
+        <hr />
+
+      
+        {
+          isLoading
+              ?<LoadingQuote />
+              :<Quote author={author} quote={quote}/>
+
+          // isLoading 
+          //     ?(
+          //       <div className="alert alert-info text-center">
+          //         Loading...
+          //       </div>
+          //     )  
+          //     :(
+          //       <blockquote className="blockquote text-end">
+          //         <p className="mb-1">{ data[0].quote }</p>
+          //         <footer className="blockquote-footer">{data[0].author}</footer>
+          //       </blockquote>                
+          //     )
+        }     
+
+        <button className="btn btn-primary" disabled={isLoading} onClick={ () => increment(1) }>
+          Next Quote
+        </button>
+
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,13 @@ import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
 import { FocusScreen } from './04-userRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
 import { HooksApp } from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <FocusScreen/>
+    <Layout/>
   //</React.StrictMode>,
 )
