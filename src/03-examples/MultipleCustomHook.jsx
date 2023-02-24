eaa6bfe4a1aa8c87893ce4ff907ae59395e506ba
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-02-23 21:28:49 -0500

    Uniendo Hooks

--- src/03-examples/MultipleCustomHook.jsx
+++ src/03-examples/MultipleCustomHook.jsx
@@ -1,12 +1,39 @@
+import { useCounter } from "../../hooks/useCounter";
 import { useFetch } from "../../hooks/useFetch"
 
 export const MultipleCustomHook = () => {
   
-    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes');
-  
+    const { counter, increment } = useCounter(1);
+    const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
+    console.log(counter)
+    const { author, quote } = !!data && data[0];
+    
     return (
     <>
         <h1>Breaking Bad Quotes </h1>
+        <hr />
+
+      
+        {
+
+          isLoading 
+              ?(
+                <div className="alert alert-info text-center">
+                  Loading...
+                </div>
+              )  
+              :(
+                <blockquote className="blockquote text-end">
+                  <p className="mb-1">{ data[0].quote }</p>
+                  <footer className="blockquote-footer">{data[0].author}</footer>
+                </blockquote>                
+              )
+        }     
+
+        <button className="btn btn-primary" disabled={isLoading} onClick={ () => increment(1) }>
+          Next Quote
+        </button>
+
     </>
   )
 }
