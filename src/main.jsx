ecdd76a40ae99475ae1ba015b9db840efceb4b9f
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-02-23 21:56:47 -0500

    Usando gitRef

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
+import { FocusScreen } from './04-userRef/FocusScreen'
 import { HooksApp } from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <MultipleCustomHook />
+    <FocusScreen/>
   //</React.StrictMode>,
 )
