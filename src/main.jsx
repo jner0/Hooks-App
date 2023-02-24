e2baa45ce326245f3f48c8602aa8d77c6e6e7a0f
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-02-23 22:46:53 -0500

    Usando Memo

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,13 @@ import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
 import { FocusScreen } from './04-userRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 import { HooksApp } from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Layout/>
+    <Memorize/>
   //</React.StrictMode>,
 )
