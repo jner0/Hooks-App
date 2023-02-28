f665814c45520bea8fbe7e12a865510fcf228c3d
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-02-27 20:55:57 -0500

    UseMemo

--- src/06-memos/Small.jsx
+++
@@ -1,9 +0,0 @@
-import {memo} from 'react';
-export const Small = React.memo(({value}) => {
-
-    console.log('Me volvi a dibujar');
-    console.log('addsaasd')
-  return (
-    <small>{ value }</small>
-  )
-})
--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,14 @@ import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHook } from './03-examples/MultipleCustomHook'
 import { FocusScreen } from './04-userRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
+//import { Memorize } from './06-memos/Memorize'
 import { HooksApp } from './HooksApp'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Memorize/>
+    <MemoHook/>
   //</React.StrictMode>,
 )
