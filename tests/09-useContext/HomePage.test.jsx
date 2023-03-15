2135e2ac3888f9a2580d28539c2c36a99fdf408b
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-14 21:56:03 -0500

    Primeras pruebas en useContext

---
+++ tests/09-useContext/HomePage.test.jsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { UserContext } from "../../src/09-useContext/context/userContext";
+import { HomePage } from "../../src/09-useContext/HomePage";
+
+describe('Pruebas en el componente HomePage', () => { 
+
+    const user = {
+        id: 1,
+        name: 'Nemecio'
+    }
+
+    test('debe de mostrar el componente sin el usuario', () => { 
+
+        render(
+        <UserContext.Provider value={{user:null}}>
+            <HomePage/>
+        </UserContext.Provider>
+        );
+
+        const preTag = screen.getByLabelText('pre'); //aria-label
+        expect( preTag.innerHTML).toBe('null');
+        //screen.debug();
+
+     });
+
+     test('debe de mostrar el componente con el usuario', () => { 
+
+        render(
+        <UserContext.Provider value={{ user }}>
+            <HomePage/>
+        </UserContext.Provider>
+        );
+
+        const preTag = screen.getByLabelText('pre'); //aria-label
+        expect( preTag.innerHTML).toContain(user.name);
+        expect( preTag.innerHTML).toContain(user.id.toString());
+        //screen.debug();
+
+     });
+
+ });
